9a6ba512c2b1a31e727f3c8b09691a8ae0a934e6
g1isgone <ji_won_chung@brown.edu>
2022-12-29 18:15:48 -0500

    (1) Factor out all sleep regularity index related calculations into a separate Utils file (2) Make a separate test case for sleep regularity index (3) Revert SocialJetlagStatsTest to original test file

--- app/src/androidTest/java/com/urbandroid/sleep/addon/stats/model/socialjetlag/SocialJetlagStatsTest.java
+++ app/src/androidTest/java/com/urbandroid/sleep/addon/stats/model/socialjetlag/SocialJetlagStatsTest.java
@@ -5,17 +5,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 import com.urbandroid.sleep.addon.stats.model.StatRecord;
 import com.urbandroid.util.ScienceUtil;
 
-import org.joda.time.Interval;
-import org.junit.Assert;
 import org.junit.Test;
 
 import java.time.Instant;
 import java.util.Arrays;
-import java.util.BitSet;
 import java.util.Date;
-import java.util.List;
 import java.util.TimeZone;
-import java.util.TreeMap;
 
 public class SocialJetlagStatsTest {
 
@@ -24,10 +19,6 @@ public class SocialJetlagStatsTest {
             new ChronoRecord(
                     Date.from(Instant.parse("2018-11-11T01:30:10Z")),
                     Date.from(Instant.parse("2018-11-11T10:00:10Z")),
-                    1.5f, 10f, 7f),
-            new ChronoRecord(
-                    Date.from(Instant.parse("2018-11-11T12:40:10Z")),
-                    Date.from(Instant.parse("2018-11-11T13:00:10Z")),
                     1.5f, 10f, 7f),
             new ChronoRecord(
                     Date.from(Instant.parse("2018-11-12T01:00:10Z")),
@@ -52,241 +43,32 @@ public class SocialJetlagStatsTest {
                     3.0f, 11f, 6f),
     }));
 
-    private SocialJetlagStats crStats = new SocialJetlagStats(cr, false);
-
     @Test
-    public void testSleepRegularityIndex() {
-        float SRIScoreOne = 1.0f - (30.0f + 20.0f)/1440.f; // Score from 2018-11-11 and 2018-11-12
-        float SRIScoreTwo = 1.0f - (60.0f)/1440.f; // Score from 2018-11-12 and 2018-11-13
-        float SRIScoreThree = 1.0f - (60.0f)/1440.f; // Score from 2018-11-24 and 2018-11-25
-        float SRIScoreFour = 1.0f - (120.0f)/1440.f; // Score from 2018-11-25 and 2018-11-26
-
-        float expectedSRIScore = (SRIScoreOne + SRIScoreTwo + SRIScoreThree + SRIScoreFour)/4.0f;
-        Assert.assertEquals(expectedSRIScore, crStats.getSleepIrregularity(), .0001);
+    public void useUtc() {
+        SocialJetlagStats  stats = new SocialJetlagStats(cr, true);
+        assertThat(stats.getSleepIrregularity()).isEqualTo(0.42305467f);
     }
 
     @Test
-    public void calculateSRIEdgeCases() {
-        // one bitset empty
-        BitSet day1Interval = new BitSet(1440);
-        BitSet day2Interval = new BitSet(); //empty set
-        day1Interval.set(0,720);
-        Assert.assertEquals(-1.0f, crStats.calculateSRI(day1Interval, day2Interval), .0001);
-        Assert.assertEquals(-1.0f, crStats.calculateSRI(day2Interval, day1Interval), .0001);
-    }
-
-    @Test public void extremeUsers(){
-        // awake 24 hours for one day and asleep 24hrs for the next
-        BitSet day1SleepStates = new BitSet(1440);
-        BitSet day2SleepStates = new BitSet(1440);
-        day1SleepStates.set(0,1440, true);
-        day2SleepStates.set(0,1440, false);
-        Assert.assertEquals(0, crStats.calculateSRI(day1SleepStates, day2SleepStates), .0001);
-
-        // awake two days in a row
-        BitSet day3SleepStates = new BitSet(1440);
-        BitSet day4SleepStates = new BitSet(1440);
-        day3SleepStates.set(0,1440, true);
-        day4SleepStates.set(0,1440, true);
-        Assert.assertEquals(1.0, crStats.calculateSRI(day3SleepStates, day4SleepStates), .0001);
-
-        // asleep two days in a row
-        BitSet day5SleepStates = new BitSet(1440);
-        BitSet day6SleepStates = new BitSet(1440);
-        day5SleepStates.set(0,1440, false);
-        day6SleepStates.set(0,1440, false);
-        Assert.assertEquals(1.0, crStats.calculateSRI(day5SleepStates, day6SleepStates), .0001);
+    public void dontUseUtc() {
+        SocialJetlagStats  stats = new SocialJetlagStats(cr, false);
+        assertThat(stats.getSleepIrregularity()).isEqualTo(0.7163131f);
     }
 
     @Test
-    public void testCalculateSRI() {
-        BitSet day1Interval = new BitSet(1440);
-        BitSet day2Interval = new BitSet(1440);
-        day1Interval.set(0,720);
-        day2Interval.set(0,720);
-        // testing for when the intevals are the same
-        float sameInterval = crStats.calculateSRI(day1Interval, day2Interval);
-        Assert.assertEquals(1.0, sameInterval, .0001);
+    public void testGetRecordIrregularity() {
 
-        // general test, one interval enclosed inside the other
-        BitSet day3Interval = new BitSet(1440);
-        day3Interval.set(0,1440);
-        Assert.assertEquals(0.5, crStats.calculateSRI(day1Interval, day3Interval), .0001);
-        Assert.assertEquals(0.5, crStats.calculateSRI(day2Interval, day3Interval), .0001);
+        SocialJetlagStats  stats = new SocialJetlagStats(cr, false);
+        assertThat(CyclicFloatKt.center(stats.getRecords().getMidSleeps(), 24)).isEqualTo(6.625f);
+        assertThat(ScienceUtil.avg(stats.getRecords().getLengths())).isEqualTo(6.5f);
 
-        // testing when intervals are completely different
-        BitSet day4Interval = new BitSet(1440);
-        day4Interval.set(720, 1440);
-        Assert.assertEquals(0, crStats.calculateSRI(day1Interval, day4Interval), .0001);
+        StatRecord record = new StatRecord(
+                Date.from(Instant.parse("2018-11-11T01:30:10Z")),
+                Date.from(Instant.parse("2018-11-11T10:00:10Z")),
+                TimeZone.getTimeZone("UTC"),
+                0, 7.0);
+        record.setTrackLengthInHours(8.5f);
 
-        // testing when there is some overlap
-        BitSet interval5 = new BitSet(1440);
-        BitSet interval6 = new BitSet(1440);
-        interval5.set(0, 720);
-        interval6.set(360, 1440);
-        Assert.assertEquals(0.25, crStats.calculateSRI(interval5, interval6), .0001);
-
-        // single overlap
-        BitSet interval7 = new BitSet(1440);
-        BitSet interval8 = new BitSet(1440);
-        interval7.set(0, 2);
-        interval8.set(1, 2);
-        Assert.assertEquals(1 - 1.0/1440.0, crStats.calculateSRI(interval7, interval8), .000001);
-
-        // pretty regular
-        BitSet interval9 = new BitSet(1440);
-        BitSet interval10 = new BitSet(1440);
-        interval9.set(0, 720);
-        interval10.set(100, 820);
-        Assert.assertEquals(1.0 - 200.0/1440.0, crStats.calculateSRI(interval9, interval10), .000001);
-    }
-
-    @Test
-    public void testCalculateAvgSRIBase() {
-        ChronoRecords halfDayDiff = new ChronoRecords(Arrays.asList(new ChronoRecord[]{
-                //The first 3 records are in UTC timezone
-                new ChronoRecord(
-                        Date.from(Instant.parse("2018-11-11T01:30:10Z")),
-                        Date.from(Instant.parse("2018-11-11T10:00:10Z")),
-                        1.5f, 10f, 7f),
-                new ChronoRecord(
-                        Date.from(Instant.parse("2018-11-12T01:00:10Z")),
-                        Date.from(Instant.parse("2018-11-12T10:00:10Z")),
-                        1.0f, 10f, 7f),}));
-        SocialJetlagStats  stats = new SocialJetlagStats(halfDayDiff, false);
-        List<Interval> recordsAsIntervals = stats.convertChronoRecordsToTimeIntervals(halfDayDiff);
-        TreeMap<String, BitSet> dayBitMap = stats.createSleepStateByDateMap(recordsAsIntervals);
-        float avgSRI = stats.calculateAverageSRI(dayBitMap);
-        Assert.assertEquals(1.0-30.0/1440.0, avgSRI, 0.001);
-    }
-
-    @Test
-    public void overLappingDays() {
-        ChronoRecords halfDayDiff = new ChronoRecords(Arrays.asList(new ChronoRecord[]{
-                //The first 3 records are in UTC timezone
-                new ChronoRecord(
-                        Date.from(Instant.parse("2018-11-11T00:00:10Z")),
-                        Date.from(Instant.parse("2018-11-11T06:00:10Z")),
-                        10f, 6f, 8f),
-                new ChronoRecord(
-                        Date.from(Instant.parse("2018-11-11T10:00:10Z")),
-                        Date.from(Instant.parse("2018-11-12T06:00:10Z")),
-                        10f, 6f, 8f),
-                new ChronoRecord(
-                        Date.from(Instant.parse("2018-11-12T10:30:10Z")),
-                        Date.from(Instant.parse("2018-11-13T06:30:10Z")),
-                        10.5f, 6.5f, 8f),
-                new ChronoRecord(
-                        Date.from(Instant.parse("2018-11-13T10:00:10Z")),
-                        Date.from(Instant.parse("2018-11-13T23:59:10Z")),
-                        10f, 6f, 8f)}));
-        SocialJetlagStats  stats = new SocialJetlagStats(halfDayDiff, false);
-        List<Interval> recordsAsIntervals = stats.convertChronoRecordsToTimeIntervals(halfDayDiff);
-        TreeMap<String, BitSet> dayBitMap = stats.createSleepStateByDateMap(recordsAsIntervals);
-        float avgSRI = stats.calculateAverageSRI(dayBitMap);
-        Assert.assertEquals(1.0-45.0/1440.0, avgSRI, 0.001);
-    }
-
-    @Test
-    public void testSkipDays() {
-        ChronoRecords halfDayDiff = new ChronoRecords(Arrays.asList(new ChronoRecord[]{
-                //The first 3 records are in UTC timezone
-                new ChronoRecord(
-                        Date.from(Instant.parse("2018-11-11T00:00:10Z")),
-                        Date.from(Instant.parse("2018-11-11T06:00:10Z")),
-                        10f, 6f, 8f),
-                new ChronoRecord(
-                        Date.from(Instant.parse("2018-11-12T00:30:10Z")),
-                        Date.from(Instant.parse("2018-11-12T06:30:10Z")),
-                        10f, 6f, 8f),
-                new ChronoRecord(
-                        Date.from(Instant.parse("2018-11-14T00:00:10Z")),
-                        Date.from(Instant.parse("2018-11-14T06:00:10Z")),
-                        10f, 6f, 8f),
-                new ChronoRecord(
-                        Date.from(Instant.parse("2018-11-15T00:30:10Z")),
-                        Date.from(Instant.parse("2018-11-15T06:30:10Z")),
-                        10f, 6f, 8f),
-        }));
-        SocialJetlagStats  stats = new SocialJetlagStats(halfDayDiff, false);
-        List<Interval> recordsAsIntervals = stats.convertChronoRecordsToTimeIntervals(halfDayDiff);
-        TreeMap<String, BitSet> dayBitMap = stats.createSleepStateByDateMap(recordsAsIntervals);
-        float avgSRI = stats.calculateAverageSRI(dayBitMap);
-
-        // SRI Score One
-        float numSRIScores = 2;
-        float sriScore1 = 1.0f-60.0f/1440.0f;
-        float sriScore2 = 1.0f-60.0f/1440.0f;
-        float expectedSRI = (sriScore1 + sriScore2)/numSRIScores;
-        Assert.assertEquals(expectedSRI, avgSRI, 0.001);
-    }
-
-    @Test // does not work when only one ChronoRecord is given
-    public void testOneEntrySleepStateMap_SRI() {
-        ChronoRecords oneDayRecord = new ChronoRecords(Arrays.asList(new ChronoRecord[]{
-                //The first 3 records are in UTC timezone
-                new ChronoRecord(
-                        Date.from(Instant.parse("2018-11-11T01:30:10Z")),
-                        Date.from(Instant.parse("2018-11-11T10:00:10Z")),
-                        1.5f, 10f, 7f)}));
-        SocialJetlagStats  stats = new SocialJetlagStats(oneDayRecord, false);
-        List<Interval> recordsAsIntervals = stats.convertChronoRecordsToTimeIntervals(oneDayRecord);
-        TreeMap<String, BitSet> dayBitMap = stats.createSleepStateByDateMap(recordsAsIntervals);
-        float avgSRI = stats.calculateAverageSRI(dayBitMap);
-        Assert.assertEquals(-1.0, avgSRI, 0.001); // should be -1 when only one chronorecord is given
-    }
-
-    @Test
-    public void testEmptySleepStatesMap_SRI() {
-        ChronoRecords emptyRecord = new ChronoRecords(Arrays.asList(new ChronoRecord[]{}));
-        SocialJetlagStats  stats = new SocialJetlagStats(emptyRecord, false);
-        List<Interval> recordsAsIntervals = stats.convertChronoRecordsToTimeIntervals(emptyRecord);
-        TreeMap<String, BitSet> dayBitMap = stats.createSleepStateByDateMap(recordsAsIntervals);
-
-        //test SRI calculation
-        float avgSRI = stats.calculateAverageSRI(dayBitMap);
-        Assert.assertEquals(-1.0, avgSRI, 0.001); // should be -1 when only one chronorecord is given
-    }
-
-    @Test
-    public void testModifiedSleepRegularityIndex(){
-        float SRIScoreOne = 1.0f - (30.0f + 20.0f)/1440.f; // Score from 2018-11-11 and 2018-11-12
-        float SRIScoreTwo = 1.0f - (60.0f)/1440.f; // Score from 2018-11-12 and 2018-11-13
-        float SRIScoreThree = 1.0f - (60.0f)/1440.f; // Score from 2018-11-24 and 2018-11-25
-        float SRIScoreFour = 1.0f - (120.0f)/1440.f; // Score from 2018-11-25 and 2018-11-26
-
-        float mSRIScoreOne = Math.abs(SRIScoreOne - SRIScoreTwo);
-        float mSRIScoreTwo = Math.abs(SRIScoreThree - SRIScoreFour);
-
-        float expectedMSRIScore = (mSRIScoreOne + mSRIScoreTwo )/2.0f;
-        Assert.assertEquals(expectedMSRIScore, crStats.getSleepIrregularityModified(), .0001);
-    }
-
-    @Test
-    public void testEmptySleepStatesMap_MSRI() {
-        ChronoRecords emptyRecord = new ChronoRecords(Arrays.asList(new ChronoRecord[]{}));
-        SocialJetlagStats  stats = new SocialJetlagStats(emptyRecord, false);
-        List<Interval> recordsAsIntervals = stats.convertChronoRecordsToTimeIntervals(emptyRecord);
-        TreeMap<String, BitSet> dayBitMap = stats.createSleepStateByDateMap(recordsAsIntervals);
-
-        //test MSRI Calculation
-        float avgMSRI = stats.calculateAverageMSRI(dayBitMap);
-        Assert.assertEquals(-1.0, avgMSRI, 0.001); // should be -1 when only one chronorecord is given
-    }
-
-    @Test // does not work when only one ChronoRecord is given
-    public void testOneEntrySleepStateMap_MSRI() {
-        ChronoRecords oneDayRecord = new ChronoRecords(Arrays.asList(new ChronoRecord[]{
-                //The first 3 records are in UTC timezone
-                new ChronoRecord(
-                        Date.from(Instant.parse("2018-11-11T01:30:10Z")),
-                        Date.from(Instant.parse("2018-11-11T10:00:10Z")),
-                        1.5f, 10f, 7f)}));
-        SocialJetlagStats  stats = new SocialJetlagStats(oneDayRecord, false);
-        List<Interval> recordsAsIntervals = stats.convertChronoRecordsToTimeIntervals(oneDayRecord);
-        TreeMap<String, BitSet> dayBitMap = stats.createSleepStateByDateMap(recordsAsIntervals);
-
-        float avgMSRI = stats.calculateAverageMSRI(dayBitMap);
-        Assert.assertEquals(-1.0, avgMSRI, 0.001); // should be -1 when only one chronorecord is given
+        assertThat(stats.getRecordIrregularity(record)).isEqualTo( ((6.625f - 3.5f) + (8.5f - 6.5f)) / 2f );
     }
 }
--- app/src/main/java/com/urbandroid/sleep/addon/stats/model/socialjetlag/SocialJetlagStats.java
+++ app/src/main/java/com/urbandroid/sleep/addon/stats/model/socialjetlag/SocialJetlagStats.java
@@ -9,27 +9,15 @@ import com.urbandroid.sleep.addon.stats.model.StatRecord;
 import com.urbandroid.sleep.addon.stats.model.socialjetlag.clustering.ClusteredChronoRecords;
 import com.urbandroid.sleep.addon.stats.model.socialjetlag.clustering.SleepLabel;
 import com.urbandroid.util.ScienceUtil;
+import com.urbandroid.sleep.addon.stats.model.socialjetlag.SleepRegularityIndexUtil;
 
 import org.apache.commons.math3.util.Pair;
 
-import org.joda.time.DateTime;
-import org.joda.time.DateTimeZone;
-import org.joda.time.Duration;
-import org.joda.time.Interval;
-
-import java.sql.Array;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.BitSet;
 import java.util.Calendar;
 import java.util.Collection;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Set;
-import java.util.TreeMap;
 
 public class SocialJetlagStats {
 
@@ -77,242 +65,6 @@ public class SocialJetlagStats {
         return new SocialJetlagStats(records.narrow(from, to), useUTCforIrregularity);
     }
 
-
-
-    /**
-     * Splits a date that over flows to the next day into two intervals
-     * @param startTime StartTime of an interval that overflows
-     * @param endTime EndTime of an interval that overflows
-     * @return Interval[] with spliced times
-     */
-    private Interval[] splitOverFlowingDate(DateTime startTime, DateTime endTime){
-        DateTime firstIntervalEndTime = new DateTime(
-                startTime.year().get(), startTime.monthOfYear().get(), startTime.dayOfMonth().get(), 23, 59
-        ).withZoneRetainFields(DateTimeZone.UTC);
-
-        DateTime secondIntervalStartTime = new DateTime(
-                endTime.year().get(), endTime.monthOfYear().get(), endTime.dayOfMonth().get(), 00, 00
-        ).withZoneRetainFields(DateTimeZone.UTC);
-
-        return new Interval[] {new Interval(startTime, firstIntervalEndTime), new Interval(secondIntervalStartTime, endTime)};
-    }
-
-    /**
-     * Checks if "nextDate" is the actual next date of prevDate
-     * @param prevDate
-     * @param nextDate
-     * @return
-     */
-    private boolean isNextDate(DateTime prevDate, DateTime nextDate){
-        DateTime actualNextDate = prevDate.plusDays(1);
-        boolean sameYear = (actualNextDate.getYear() == nextDate.getYear());
-        boolean sameMonth = (actualNextDate.getMonthOfYear() == nextDate.getMonthOfYear());
-        boolean sameDate = (actualNextDate.getDayOfMonth() == nextDate.getDayOfMonth());
-
-        return sameYear && sameMonth && sameDate;
-    }
-
-    /**
-     * (1) Converts ChronoRecords to Joda Time Intervals - easier to manipulate than "Date"
-     * (2) Splices Time Intervals Overflowing to Next Day
-     *     (i.e. [Monday 8pm - Tuesday 2am] --> [Monday 8pm - Monday 11:59pm], [Tuesday 12 - 2am]
-     * @param records ChronoRecords including awake times
-     * @return nonOverFlowingTimeIntervals
-     */
-    public List<Interval> convertChronoRecordsToTimeIntervals(ChronoRecords records ){
-        List<ChronoRecord> listOfRecords = records.getRecordsList();
-        List<Interval> nonOverFlowingTimeIntervals = new ArrayList<Interval>();
-
-        for(int i = 0; i< listOfRecords.size(); i++){
-            ChronoRecord cr1 = listOfRecords.get(i);
-
-            DateTime intervalStart = new DateTime(cr1.getFrom()).withZone(DateTimeZone.UTC);
-            DateTime intervalEnd = new DateTime(cr1.getTo()).withZone(DateTimeZone.UTC);
-
-            if(isNextDate(intervalStart, intervalEnd)){
-                // Split interval to two, i1, i2
-                Interval[] splitIntervals = splitOverFlowingDate(intervalStart, intervalEnd);
-                nonOverFlowingTimeIntervals.add(splitIntervals[0]);
-                nonOverFlowingTimeIntervals.add(splitIntervals[1]);
-            }else{
-                nonOverFlowingTimeIntervals.add(new Interval(intervalStart, intervalEnd));
-            }
-        }
-
-        return nonOverFlowingTimeIntervals;
-    }
-
-    /**
-     * (1) Store in values awakeTimes as a 1440 (24 hrs x 60 min) sized bitset
-     *     such that each bitset value is non-empty if in "awake-state"
-     *     0 if in "sleep-state"
-     * (2) Key is the date in String format yyyy-MM-dd
-     * (3) Used for easy cross-day comparison of intersecting time intervals
-     * @param awakeTimes List of intervals that indicate user "awake" state
-     * @return sleepStateByDateMap
-     */
-    public TreeMap<String, BitSet> createSleepStateByDateMap(List<Interval> awakeTimes){
-        TreeMap<String, BitSet> sleepStateByDateMap = new TreeMap<>();
-        for(int j = 0; j < awakeTimes.size(); ++j){
-            Interval awakeInterval = awakeTimes.get(j);
-
-            BitSet sleepState;
-
-            DateTime awakeIntervalStartTime = awakeInterval.getStart();
-            String awakeIntervalFrom = awakeIntervalStartTime.toString("yyyy-MM-dd");
-
-            if(sleepStateByDateMap.containsKey(awakeIntervalFrom)){
-                sleepState = sleepStateByDateMap.get(awakeIntervalFrom);
-            } else{
-                sleepState = new BitSet(1440); // 24 hrs * 60 minutes
-            }
-
-            int startIdx = awakeIntervalStartTime.getMinuteOfDay();
-            int endIdx = awakeInterval.getEnd().getMinuteOfDay();
-            sleepState.set(startIdx, endIdx);
-
-            sleepStateByDateMap.put(awakeIntervalFrom, sleepState);
-        }
-
-        return sleepStateByDateMap;
-    }
-
-    /**
-     * Calculates SRI score given 2 days
-     * It is the (total number of minutes in which a user is in a consistent sleep state) / 1440 mins
-     * Consistent state: prevDay[i] == nextDay[i]
-     * Inconsistent state: prevDay[i] =/= nextDay[i]
-     * @param prevDay
-     * @param nextDay
-     * @return
-     */
-    public float calculateSRI(BitSet prevDay, BitSet nextDay){
-        // Make sure not to use isEmpty as that checks if there are no falses
-        if(prevDay.size() < 1440 || nextDay.size() < 1440 ){
-            return -1.0f;
-        }
-        BitSet minsInconsistentSleepState = (BitSet) prevDay.clone(); //store inconsistent sleep state mins
-        minsInconsistentSleepState.xor(nextDay); // true means prevDay(i) and nextDay(i) had different values
-
-        return (1.0f - (minsInconsistentSleepState.cardinality()/1440.f));
-    }
-
-    /**
-     * Extracts SRI scores and groups them by continuous segments in a HashMap
-     * @param sleepStateByDateMap
-     * @return
-     */
-    public HashMap<Integer, List<Float>> extractSRIScores(TreeMap<String, BitSet> sleepStateByDateMap){
-        HashMap<Integer, List<Float>> groupedSRIScores = new HashMap<Integer, List<Float>>();
-
-        Set keys = sleepStateByDateMap.keySet();
-        int groupNum = 0;
-
-        Iterator nextDayIt = keys.iterator();
-        // iterate through pairs of days to calculate the SRI
-        if (nextDayIt.hasNext()) {
-            nextDayIt.next();
-            for (Iterator prevDayIt = keys.iterator(); prevDayIt.hasNext() && nextDayIt.hasNext();) {
-
-                String prevDateKey = (String) prevDayIt.next();
-                String nextDateKey =  (String) nextDayIt.next();
-                DateTime prevDate = new DateTime(prevDateKey);
-                DateTime nextDate = new DateTime(nextDateKey);
-
-                if(isNextDate(prevDate, nextDate)){ // if there are a continuous pair of days, calculate the SRI
-
-                    BitSet prevDaySleepStates = sleepStateByDateMap.get(prevDateKey);
-                    BitSet nextDaySleepStates = sleepStateByDateMap.get(nextDateKey);
-
-                    // extract the date and month and if it is one
-                    float sriScore = calculateSRI(prevDaySleepStates, nextDaySleepStates);
-
-                    // insert score into corresponding group
-                    if(groupedSRIScores.containsKey(groupNum)){
-                        groupedSRIScores.get(groupNum).add(sriScore);
-                    } else {
-                        LinkedList<Float> listOfSRIScores = new LinkedList<Float>();
-                        listOfSRIScores.add(sriScore);
-                        groupedSRIScores.put(groupNum, listOfSRIScores);
-                    }
-                } else { // Dates are no longer continuous, make a new group
-                    groupNum++;
-                    System.out.println("Skipping SRI score calculation for following pair of dates. Date ranges must be contiguous to compute the SRI. User provided date ranges: " + prevDateKey + "- " + nextDateKey);
-                }
-            }
-        }
-        return groupedSRIScores;
-    }
-
-    /**
-     * Calculates average SRI across multiple, continuous dates
-     * @param sleepStateByDateMap
-     * @return
-     */
-    public float calculateAverageSRI(TreeMap<String, BitSet> sleepStateByDateMap){
-        Set keys = sleepStateByDateMap.keySet();
-
-        if (keys.size() < 2){ // there should be at least two dates to calculate the SRI
-            return -1.0f;
-        }
-
-        HashMap<Integer, List<Float>> groupedSRISCores = extractSRIScores(sleepStateByDateMap);
-
-        float cumulativeSRI = 0;
-        int numSRIScores = 0;
-
-        for ( List<Float> sriScores : groupedSRISCores.values()) {
-            final Iterator<Float> scoreIterator = sriScores.listIterator();
-            while(scoreIterator.hasNext()){
-                cumulativeSRI += scoreIterator.next();
-                numSRIScores++;
-            }
-        }
-        return cumulativeSRI / numSRIScores;
-    }
-
-    /**
-     * Calculates average mSRI across multiple, continuous dates
-     * The mSRI is the cumulative difference between pairs of SRI scores
-     * @param sleepStateByDateMap
-     * @return
-     */
-    public float calculateAverageMSRI(TreeMap<String, BitSet> sleepStateByDateMap){
-        Set keys = sleepStateByDateMap.keySet();
-
-        if (keys.size() < 3){ // there should be at least three dates to calculate the mSRI
-            return -1.0f;
-        }
-
-        HashMap<Integer, List<Float>> groupedSRISCores = extractSRIScores(sleepStateByDateMap);
-
-        float cumulativeMSRI = 0;
-        int numMSRIScores = 0;
-
-        for ( List<Float> sriScores : groupedSRISCores.values()) {
-            // if there are at least two sri scores, calculate the msri; else skip
-            if(sriScores.size() >= 2){
-                final Iterator<Float> scoreIterator = sriScores.listIterator();
-                float currSRI = scoreIterator.next();
-                while(scoreIterator.hasNext()){
-                    float nextSRI = scoreIterator.next();
-                    float mSRI = Math.abs(currSRI - nextSRI);
-                    cumulativeMSRI += mSRI;
-                    numMSRIScores++;
-                    currSRI = nextSRI; // update pointer
-                }
-            }
-        }
-
-        return (numMSRIScores == 0 ) ? -1.0f : cumulativeMSRI / numMSRIScores;
-    }
-
-    public float getSleepIrregularityModified(){
-        List<Interval> awakeTimes = convertChronoRecordsToTimeIntervals(records);
-        TreeMap<String, BitSet> sleepStateByDateMap = createSleepStateByDateMap(awakeTimes);
-        return calculateAverageMSRI(sleepStateByDateMap);
-    }
-
     public float getSleepIrregularity() {
         return valueCache.computeIfAbsent("SleepIrregularity", new ValueCache.Supplier() {
             @Override
@@ -320,11 +72,12 @@ public class SocialJetlagStats {
                 if (records.size() < 5) {
                     return -1f;
                 } else {
-
-                    List<Interval> awakeTimes = convertChronoRecordsToTimeIntervals(records);
-                    TreeMap<String, BitSet> sleepStateByDateMap = createSleepStateByDateMap(awakeTimes);
-
-                    return calculateAverageSRI(sleepStateByDateMap);
+                    SleepRegularityIndexUtil sriUtils = new SleepRegularityIndexUtil();
+                    return sriUtils.getSleepIrregularity(records);
+//                    float[] midSleeps = useUTCforIrregularity ? records.getMidSleepUTC() : records.getMidSleeps();
+//                    float midSleepStd = CyclicFloatKt.stdev(midSleeps, 24f);
+//                    float sleepLenStd = ScienceUtil.stddev(records.getLengths());
+//                    return (midSleepStd + sleepLenStd) / 2;
                 }
             }
         });
